c1751f78ce35e954aac366f7605027e758353054
Megyesi Albert <megyesi.albert@gmail.com>
2019-05-27 16:33:04 +0200

    Felesleges oszlopok elhagyása

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ import matplotlib.pyplot as plot
 from matplotlib.pyplot import figure
 
 gazellatas = pan.read_excel('./data/2_3_9i.xls', sheet_name = '2.3.9.', skiprows = [0, 1])
+gazellatas = gazellatas.drop(gazellatas.columns[[1, 3, 4, 5, 6]], axis = 1)

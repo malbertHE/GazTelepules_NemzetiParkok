a25262e41a20ce3b1553f5491d128fc927c06680
Megyesi Albert <megyesi.albert@gmail.com>
2019-05-27 16:37:10 +0200

    Fejlécek nevet kapnak

--- main.py
+++ main.py
@@ -4,3 +4,4 @@ from matplotlib.pyplot import figure
 
 gazellatas = pan.read_excel('./data/2_3_9i.xls', sheet_name = '2.3.9.', skiprows = [0, 1])
 gazellatas = gazellatas.drop(gazellatas.columns[[1, 3, 4, 5, 6]], axis = 1)
+gazellatas = gazellatas.rename(index = str, columns = {gazellatas.columns[0]: 'evszam', gazellatas.columns[1]: 'gaz'})

8d91e5fe8448e44be382a11b411337395e1b9a83
Megyesi Albert <megyesi.albert@gmail.com>
2019-05-27 16:31:06 +0200

    Gázellátás adatok beolvasása excelbõl

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 import pandas as pan
 import matplotlib.pyplot as plot
 from matplotlib.pyplot import figure
+
+gazellatas = pan.read_excel('./data/2_3_9i.xls', sheet_name = '2.3.9.', skiprows = [0, 1])

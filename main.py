0a1e68311e5f9cb6e71e3c001cd904e463e6a9b4
Megyesi Albert <megyesi.albert@gmail.com>
2019-05-27 16:39:02 +0200

    Nemzeti park adatok beolvasása excelbõl

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ from matplotlib.pyplot import figure
 gazellatas = pan.read_excel('./data/2_3_9i.xls', sheet_name = '2.3.9.', skiprows = [0, 1])
 gazellatas = gazellatas.drop(gazellatas.columns[[1, 3, 4, 5, 6]], axis = 1)
 gazellatas = gazellatas.rename(index = str, columns = {gazellatas.columns[0]: 'evszam', gazellatas.columns[1]: 'gaz'})
+
+elhagyandoSorok = []
+for i in range(0, 20) :
+    elhagyandoSorok.append(i)
+for i in range(36, 55) :
+    elhagyandoSorok.append(i)
+nemzetipark = pan.read_excel('./data/5_2_2i.xls', sheet_name = '5.2.2.', skiprows = elhagyandoSorok)
